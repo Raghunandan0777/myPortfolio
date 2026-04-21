0717c16c6c45d3af5cde177c6f7e237123ae593a
Raghunandan Shah <Raghunandanshah908014@gmail.com>
2026-04-21 16:11:07 +0530

    added project

--- src/data/projects.js
+++ src/data/projects.js
@@ -37,8 +37,43 @@ export const projects = [
         featured: true,
         category: "FullStack",
     },
-];
 
+    
+    {
+       id: 4,
+       title: "LMS",
+       description: "Built an LMS website using the MERN stack. With proper course management and secure user authentication. and admin panel for course management.",
+       image: "/LMS.png",
+       techStack: ["React.js", "Node.js", "MongoDB", "Express.js","Tailwind CSS"],
+       liveUrl: "https://lms-complete-project.onrender.com",
+       githubUrl: "https://github.com/Raghunandan0777/LMS-Project",
+       category: "FullStack",
+       featured: true,
+   },
+    {
+       id: 5,
+       title: "GST-Invoice-generator",
+       description: "Built an GST Invoice generator  its helps the small business to generate gst invoice and manage their customers and products. in one minuts",
+       image: "/Gst.png",
+       techStack: ["Next.js", "Node.js", "superbase", "Express.js","Tailwind CSS","typescript"],
+       liveUrl: "https://gst-invoice-generator-three.vercel.app",
+       githubUrl: "https://github.com/Raghunandan0777/GST-Invoice-generator",
+       category: "FullStack",
+       featured: true,
+   },
+
+   {
+       id: 6,
+       title: "LinkVault",
+       description: "Built an LinkVault its helps the creater to save their likns and share with their followers",
+       image: "/linkvault.png",
+       techStack: ["Next.js", "Node.js", "superbase", "Express.js","Tailwind CSS","typescript"],
+       liveUrl: "https://linkvault-nubz.onrender.com",
+       githubUrl: "https://github.com/Raghunandan0777/LinkVault",
+       category: "FullStack",
+       featured: true,
+   },
+];
 // Get featured projects for hero/highlights
 export const getFeaturedProjects = () => projects.filter(p => p.featured);
 
